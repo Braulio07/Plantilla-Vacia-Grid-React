f87cd7cdaf29f0f748c3191968f92e0224ab4f74
Braulio Ovalles <braulioovalles7d@outlook.com>
2021-12-28 01:54:45 -0400

    plantilla ordenada con grid y lineas de columnas, esta es la forma recomienda

--- src/components/Home/HomeElements.js
+++ src/components/Home/HomeElements.js
@@ -8,125 +8,135 @@ import styled from "styled-components";
 
 
 export const Container = styled.div`
-            width: 95%;
-  
-            /* max-width: 1000px; */
-            margin: 20px auto;
-            display: grid;
-            grid-gap: 10px;
-            grid-template-columns: repeat(3, 1fr);
-            grid-template-rows: repeat(4, auto);
-            background-color: #fff;
+        width: 95%;
 
-  
-                `
+        /* max-width: 1000px; */
+        margin: 20px auto;
+        display: grid;
+        grid-gap: 10px;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: repeat(4, auto);
+        background-color: #fff;
+
+        grid-template-areas: 
+            "header header header"
+            "body body sidebar"
+            "widget-1 widget-2 sidebar"
+            "footer footer footer";
+
+            @media screen and (max-width: 768px){
+            grid-template-areas:
+            "header header header"
+            "body body body"
+            "widget-1 widget-1 widget-1"
+            "widget-2 widget-2 widget-2"
+            "footer footer footer";
+
+            }
+        `
 
 export const Header = styled.header`
-                    background-color: black;
-                    color: white;
-                    padding: 20px;
-                    border-radius: 4px;
-                    grid-column-start: 1;
-                    grid-column-end: -1;
-                    
-                    
-                `
+            background-color: black;
+            color: white;
+            padding: 20px;
+            border-radius: 4px;
+
+            grid-area: header;
+            
+            
+        `
 
 
 export const IconoHeader = styled.div`
+        display: none;
+        color: yellow;
 
-display: none;
-color: yellow;
-
-@media screen and (max-width: 768px){
-    display: inline;
-}
-`
+        @media screen and (max-width: 768px){
+        display: flex;
+        justify-content: right;
+        }
+        `
 
 export const Body = styled.div`
-                    grid-column-start: 1;
-                    grid-column-end: 3;
+        grid-area: body;
 
-
-                    @media screen and (max-width: 768px){
-                        grid-column-end: 4;
-                    }
-                `
+            @media screen and (max-width: 768px){
+                grid-column-end: 4;
+            }
+        `
 
 export const H1 = styled.h1`
-                    font-size: 18px;
-                    font-weight: bold;
-                `
+            font-size: 18px;
+            font-weight: bold;
+        `
 
 export const H2 = styled.h2`
-                    font-size: 16px;
-                    font-weight: bold;
-                `
+            font-size: 16px;
+            font-weight: bold;
+        `
 
 export const H3 = styled.h3`
-                    font-size: 14px;
-                    font-weight: bold;
-                `
+            font-size: 14px;
+            font-weight: bold;
+        `
 
 export const P = styled.p`
-                    font-size: 14px;
-                `
+            font-size: 14px;
+        `
 
 
 export const SideBar = styled.aside`
-                    background-color: #EA5C2B;
-                    color: #fff;
-                    padding: 20px;
-                    border-radius: 4px;
-                    grid-column: 3 / 4;
-                    grid-row: 2 / -1;
-                    text-align: center;
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
+            background-color: #EA5C2B;
+            color: #fff;
+            padding: 20px;
+            border-radius: 4px;
+            text-align: center;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            grid-area: sidebar;
+
+            @media screen and (max-width: 768px){
+                background-color: black;
+                display: none;
+            }
 
 
-                    @media screen and (max-width: 768px){
-                        background-color: black;
-                        display: none;
-                    }
-        
-
-                `
+        `
 
 export const WidGet1 = styled.div`
-                    background-color: #548CFF;
-                    padding: 20px;
-                    border-radius: 4px;
-                    grid-column: 1 / 1;
-                    height: 100px;
-                    text-align: center;
-                    color: #fff;
-                    text-align: center;
-                    @media screen and (max-width: 768px){
-                        grid-column-end: 4;
-                    }
-                `
+            background-color: #548CFF;
+            padding: 20px;
+            border-radius: 4px;
+            grid-area: widget-1;
+            height: 100px;
+            text-align: center;
+            color: #fff;
+            text-align: center;
+            @media screen and (max-width: 768px){
+
+            }
+        `
 export const WidGet2 = styled.div`
-                background-color: #548CFF;
-                padding: 20px;
-                    border-radius: 4px;
-                    grid-column: 2 / 2;
-                    height: 100px;
-                    text-align: center;
-                    color: #fff;
-                    text-align: center;
-                    @media screen and (max-width: 768px){
-                        grid-column: 1 / 4;
-                    }
-                `
+        background-color: #548CFF;
+        padding: 20px;
+            border-radius: 4px;
+        grid-area: widget-2;
+            height: 100px;
+            text-align: center;
+            color: #fff;
+            text-align: center;
+            @media screen and (max-width: 768px){
+
+            }
+        `
 
 
 export const Footer = styled.div`
-                background-color: black;
-                color: white;
-                padding: 20px;
-                    border-radius: 4px;
-                    grid-column-start: 1;
-                    grid-column-end: 4;
-                `
+        background-color: black;
+        color: white;
+        padding: 20px;
+        grid-area: footer;
+        border-radius: 4px;
+        ` 
