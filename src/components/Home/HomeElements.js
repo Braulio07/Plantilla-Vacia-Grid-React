132bbb047f4d9c7085de377d6d26b69cc41ea6b7
Braulio Ovalles <braulioovalles7d@outlook.com>
2021-12-28 01:19:59 -0400

    plantilla ordenada con grid y columnas establecidas, no se recomienda

--- src/components/Home/HomeElements.js
+++ src/components/Home/HomeElements.js
@@ -1,51 +1,132 @@
 import styled from "styled-components";
 
 
+// grid-gap:-------------------------------------> aplica un margen a cada uno de los elementos.
+// grid-template-columns: repeat(3, 1fr); ------>  cantidad de columnas:  
+
+
 
 
 export const Container = styled.div`
+            width: 95%;
+  
+            /* max-width: 1000px; */
+            margin: 20px auto;
+            display: grid;
+            grid-gap: 10px;
+            grid-template-columns: repeat(3, 1fr);
+            grid-template-rows: repeat(4, auto);
+            background-color: #fff;
 
-    `
+  
+                `
 
 export const Header = styled.header`
-        background-color: black;
-        color: white;
-    `
+                    background-color: black;
+                    color: white;
+                    padding: 20px;
+                    border-radius: 4px;
+                    grid-column-start: 1;
+                    grid-column-end: -1;
+                    
+                    
+                `
+
+
+export const IconoHeader = styled.div`
+
+display: none;
+color: yellow;
+
+@media screen and (max-width: 768px){
+    display: inline;
+}
+`
 
 export const Body = styled.div`
+                    grid-column-start: 1;
+                    grid-column-end: 3;
 
-    `
+
+                    @media screen and (max-width: 768px){
+                        grid-column-end: 4;
+                    }
+                `
 
 export const H1 = styled.h1`
-        font-size: 16px;
-        font-weight: bold;
-    `
+                    font-size: 18px;
+                    font-weight: bold;
+                `
 
 export const H2 = styled.h2`
-        font-size: 14px;
-        font-weight: bold;
-    `
+                    font-size: 16px;
+                    font-weight: bold;
+                `
 
+export const H3 = styled.h3`
+                    font-size: 14px;
+                    font-weight: bold;
+                `
 
 export const P = styled.p`
-        font-size: 14px;
-    `
+                    font-size: 14px;
+                `
 
 
-export const SideBar = styled.div`
-        background-color: orange;
+export const SideBar = styled.aside`
+                    background-color: #EA5C2B;
+                    color: #fff;
+                    padding: 20px;
+                    border-radius: 4px;
+                    grid-column: 3 / 4;
+                    grid-row: 2 / -1;
+                    text-align: center;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
 
-    `
+
+                    @media screen and (max-width: 768px){
+                        background-color: black;
+                        display: none;
+                    }
+        
+
+                `
 
 export const WidGet1 = styled.div`
-        background-color: blue;
-    `
+                    background-color: #548CFF;
+                    padding: 20px;
+                    border-radius: 4px;
+                    grid-column: 1 / 1;
+                    height: 100px;
+                    text-align: center;
+                    color: #fff;
+                    text-align: center;
+                    @media screen and (max-width: 768px){
+                        grid-column-end: 4;
+                    }
+                `
 export const WidGet2 = styled.div`
-       background-color: blue;
-    `
+                background-color: #548CFF;
+                padding: 20px;
+                    border-radius: 4px;
+                    grid-column: 2 / 2;
+                    height: 100px;
+                    text-align: center;
+                    color: #fff;
+                    text-align: center;
+                    @media screen and (max-width: 768px){
+                        grid-column: 1 / 4;
+                    }
+                `
 
 
 export const Footer = styled.div`
-      background-color: black;
-      color: white;
-    `
+                background-color: black;
+                color: white;
+                padding: 20px;
+                    border-radius: 4px;
+                    grid-column-start: 1;
+                    grid-column-end: 4;
+                `
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-import { Container, Header, Body, H1, H2, P, SideBar, WidGet1, WidGet2, Footer } from './HomeElements';
+import { Container, Header,IconoHeader, Body, H1, H2, H3, P, SideBar, WidGet1, WidGet2, Footer } from './HomeElements';
 
 
 
@@ -11,14 +11,20 @@ export const Home = () => {
     return (
         <Container>
             <Header>
-                Header
+                <H3>  Header </H3>
+
+                <IconoHeader>IconoMenu</IconoHeader>
             </Header>
 
             <Body>
-                <H1>Bienvenidos</H1>
+                <H1>Contenido</H1>
 
+
+                {/* Lorem 1 */}
+
+                <H2> ¿Qué es Lorem Ipsum? </H2>
                 <P>
-                    <H2> ¿Qué es Lorem Ipsum? </H2>
+
                     Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.
                     Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500,
                     cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una
@@ -28,14 +34,58 @@ export const Home = () => {
                     creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más
                     recientemente con software de autoedición, como por ejemplo Aldus PageMaker,
                     el cual incluye versiones de Lorem Ipsum.</P>
-             
-                <SideBar>SiderBar</SideBar>
+                <br /><br />
+
+                {/* Lorem 2 */}
+                              <H2> ¿Qué es Lorem Ipsum? </H2>
+                <P>
+      
+                    Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.
+                    Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500,
+                    cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una
+                    galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.
+                    No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos
+                    electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la
+                    creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más
+                    recientemente con software de autoedición, como por ejemplo Aldus PageMaker,
+
+                    el cual incluye versiones de Lorem Ipsum.</P>
+                    <br /><br />
 
 
-                <WidGet1>WidGet 1</WidGet1>
-                <WidGet2>WidGet 2</WidGet2>
+                {/* Lorem 3 */}
+                <H2> ¿Qué es Lorem Ipsum? </H2>
+                <P>
+      
+                    Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.
+                    Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500,
+                    cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una
+                    galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.
+                    No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos
+                    electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la
+                    creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más
+                    recientemente con software de autoedición, como por ejemplo Aldus PageMaker,
+
+                    el cual incluye versiones de Lorem Ipsum.</P>
+                    <br /><br />
 
             </Body>
+
+
+
+            <SideBar>
+                <H3> SiderBar    </H3>
+            </SideBar>
+
+
+            <WidGet1>
+                <H3>WidGet 1</H3>
+            </WidGet1>
+
+            <WidGet2>
+                <H3>WidGet 2</H3>
+            </WidGet2>
+
 
 
             <Footer> Footer </Footer>
