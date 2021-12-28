277982a9679f8544df7af6b8bfd0908a37bc28b0
Braulio Ovalles <braulioovalles7d@outlook.com>
2021-12-28 00:05:58 -0400

    plantilla modelo vacia

---
+++ src/components/Home/HomeElements.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+
+
+
+export const Container = styled.div`
+
+    `
+
+export const Header = styled.header`
+        background-color: black;
+        color: white;
+    `
+
+export const Body = styled.div`
+
+    `
+
+export const H1 = styled.h1`
+        font-size: 16px;
+        font-weight: bold;
+    `
+
+export const H2 = styled.h2`
+        font-size: 14px;
+        font-weight: bold;
+    `
+
+
+export const P = styled.p`
+        font-size: 14px;
+    `
+
+
+export const SideBar = styled.div`
+        background-color: orange;
+
+    `
+
+export const WidGet1 = styled.div`
+        background-color: blue;
+    `
+export const WidGet2 = styled.div`
+       background-color: blue;
+    `
+
+
+export const Footer = styled.div`
+      background-color: black;
+      color: white;
+    `
